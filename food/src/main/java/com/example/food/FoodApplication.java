067dc4b65191b676017bb30a2ffb570b08c2685c
williamrobinson2954 <44370828+williamrobinson2954@users.noreply.github.com>
2018-12-12 16:55:14 -0600

    code

---
+++ food/src/main/java/com/example/food/FoodApplication.java
@@ -0,0 +1,38 @@
+package com.example.food;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApplication.class, args);
+	}
+
+	
+	
+	
+	
+	@RefreshScope
+	@RestController
+	class MessageRestController{
+	
+		@Value("${message:Hello Default}")
+		private String message;
+		
+		@RequestMapping("/message")
+		String getMessage() {
+			return this.message;
+		}
+		
+	}
+	
+	
+	
+}
+
